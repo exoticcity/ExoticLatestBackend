4b0c4f2a83f07e78db534e00dc35e79b8d0b8ab4
exoticcity <exoticcity121@gmail.com>
2025-02-13 12:25:33 +0500

    Update scheduler.py
    changed job time

--- items/scheduler.py
+++ items/scheduler.py
@@ -1,18 +1,32 @@
-from datetime import datetime
+# from datetime import datetime
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from .token import *
+ 
+# def start():
+#     scheduler = BackgroundScheduler()
+#     # scheduler.add_job(sehedule_api, 'cron', day='1', hour='0', minute='6')
+#     scheduler.add_job(updateJobs, 'interval',seconds=3600)
+#     scheduler.add_job(updateItemsalepriceJobs, 'interval',seconds=80000)
+#     # scheduler.add_job(sehedule_api, 'interval',seconds=5)
+#     scheduler.start()
+ 
+#     # # Manually trigger the job
+#     # sehedule_api()
+ 
+# # Call the start function to begin scheduling the job
+# start()
 from apscheduler.schedulers.background import BackgroundScheduler
 from .token import *
- 
+
 def start():
     scheduler = BackgroundScheduler()
-    # scheduler.add_job(sehedule_api, 'cron', day='1', hour='0', minute='6')
-    scheduler.add_job(updateJobs, 'interval',seconds=3600)
-    scheduler.add_job(updateItemsalepriceJobs, 'interval',seconds=80000)
-    # scheduler.add_job(sehedule_api, 'interval',seconds=5)
+    
+    # Schedule jobs to run every 5 minutes (300 seconds)
+    scheduler.add_job(updateJobs, 'interval', seconds=300)  # Every 5 minutes
+    scheduler.add_job(updateItemsalepriceJobs, 'interval', seconds=300)  # Every 5 minutes
+    
     scheduler.start()
- 
-    # # Manually trigger the job
-    # sehedule_api()
- 
+
 # Call the start function to begin scheduling the job
 start()
  
